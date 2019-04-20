af7888b90e79ae794a83b0942ad20d7fc8482c1f
hy3n4 <hyena@shieldspeak.com>
2019-04-20 05:46:07 +0200

    First Commit!

---
+++ Main.Designer.vb
@@ -0,0 +1,113 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Main
+    Inherits System.Windows.Forms.Form
+
+    'Form esegue l'override del metodo Dispose per pulire l'elenco dei componenti.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Richiesto da Progettazione Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: la procedura che segue è richiesta da Progettazione Windows Form
+    'Può essere modificata in Progettazione Windows Form.  
+    'Non modificarla mediante l'editor del codice.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Main))
+        Me.VersionLabel = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TarkovTimer = New System.Windows.Forms.Timer(Me.components)
+        Me.SettingsTimer = New System.Windows.Forms.Timer(Me.components)
+        Me.ExitTimer = New System.Windows.Forms.Timer(Me.components)
+        Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
+        Me.StartTimer = New System.Windows.Forms.Timer(Me.components)
+        Me.FolderBrowserDialog1 = New System.Windows.Forms.FolderBrowserDialog()
+        Me.SuspendLayout()
+        '
+        'VersionLabel
+        '
+        Me.VersionLabel.AutoSize = True
+        Me.VersionLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.VersionLabel.Location = New System.Drawing.Point(158, 55)
+        Me.VersionLabel.Name = "VersionLabel"
+        Me.VersionLabel.Size = New System.Drawing.Size(105, 20)
+        Me.VersionLabel.TabIndex = 5
+        Me.VersionLabel.Text = "v0.1 ALPHA"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Tai Le", 26.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.Label2.Location = New System.Drawing.Point(-3, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(274, 45)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "E4GL3 TARKOV"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Tai Le", 24.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.Label1.Location = New System.Drawing.Point(-4, 41)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(156, 43)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "ESCAPRE"
+        '
+        'TarkovTimer
+        '
+        Me.TarkovTimer.Interval = 1000
+        '
+        'SettingsTimer
+        '
+        Me.SettingsTimer.Interval = 30000
+        '
+        'ExitTimer
+        '
+        Me.ExitTimer.Interval = 1000
+        '
+        'StartTimer
+        '
+        Me.StartTimer.Interval = 3000
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(266, 89)
+        Me.Controls.Add(Me.VersionLabel)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents VersionLabel As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TarkovTimer As Timer
+    Friend WithEvents SettingsTimer As Timer
+    Friend WithEvents ExitTimer As Timer
+    Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
+    Friend WithEvents StartTimer As Timer
+    Friend WithEvents FolderBrowserDialog1 As FolderBrowserDialog
+End Class
---
+++ Main.vb
@@ -0,0 +1,136 @@
+﻿
+Public Class Main
+    Public path As String
+    Public exePath As String
+    Public appPath As String
+    Public tarkovPath As String
+
+
+
+    Private Sub SettingsTimer_Tick(sender As Object, e As EventArgs) Handles SettingsTimer.Tick
+        SettingsTimer.Enabled = False
+        Dim inipath As String = My.Computer.FileSystem.SpecialDirectories.MyDocuments & "\Escape From Tarkov\local.ini"
+        FileOpen(1, inipath, OpenMode.Binary)
+        Lock(1)
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.CenterToScreen()
+        CheckForUpdates()
+        CheckDirectory()
+        'Me.Visible = False
+        StartTimer.Enabled = True
+    End Sub
+    Private Sub StartTimer_Tick(sender As Object, e As EventArgs) Handles StartTimer.Tick
+        StartTimer.Enabled = False
+        Dim q() As Process
+        q = Process.GetProcessesByName("EscapeFromTarkov")
+        If q.Count > 0 Then
+            ' Process is running
+
+            MsgBox("Please close the game!", 16, "Error")
+            Close()
+        Else
+            ' Process is not running
+            '   Dim tarkovpath As String = "E:\Battlestate Games\BsgLauncher\BsgLauncher.exe"
+            Dim proc As New System.Diagnostics.Process()
+            proc = Process.Start(tarkovPath)
+            TarkovTimer.Enabled = True
+        End If
+    End Sub
+    Private Sub TarkovTimer_Tick(sender As Object, e As EventArgs) Handles TarkovTimer.Tick
+        Dim p() As Process
+        Dim z() As Process
+        p = Process.GetProcessesByName("EscapeFromTarkov")
+        z = Process.GetProcessesByName("BsgLauncher")
+        If p.Count > 0 Then
+            ' Process is running
+            'MsgBox("Running!")
+            TarkovTimer.Enabled = False
+            ExitTimer.Enabled = True
+            SettingsTimer.Enabled = True
+        Else
+            If z.Count > 0 Then
+            Else
+                Close()
+            End If
+        End If
+    End Sub
+
+    Private Sub ExitTimer_Tick(sender As Object, e As EventArgs) Handles ExitTimer.Tick
+        Dim w() As Process
+        w = Process.GetProcessesByName("EscapeFromTarkov")
+        If w.Count > 0 Then
+            ' Process is running
+        Else
+            ' Process is not running
+            ExitTimer.Enabled = False
+            Close()
+        End If
+    End Sub
+
+    Public Sub CheckForUpdates()
+        VersionLabel.Text = "v" & Application.ProductVersion & " ALPHA"
+        exePath = Application.ExecutablePath()
+        appPath = Application.StartupPath()
+        My.Computer.Registry.SetValue("HKEY_CURRENT_USER\E4GL3\ETE", "exePath", exePath)
+        My.Computer.Registry.SetValue("HKEY_CURRENT_USER\E4GL3\ETE", "appPath", appPath)
+        'MsgBox(appPath)
+
+        Dim x As Integer
+        Dim paths() As String = IO.Directory.GetFiles(appPath, "updatre.exe")
+        If paths.Length > 0 Then
+            For x = 0 To paths.Length - 1
+                IO.File.Delete(paths(x))
+            Next
+        End If
+
+        Dim request As System.Net.HttpWebRequest = System.Net.HttpWebRequest.Create("http://shieldspeak.com/eagle/version")
+        Dim response As System.Net.HttpWebResponse = request.GetResponse()
+
+        Dim sr As System.IO.StreamReader = New System.IO.StreamReader(response.GetResponseStream())
+
+        Dim newestversion As String = sr.ReadToEnd()
+        Dim currentversion As String = Application.ProductVersion
+
+        If newestversion.Contains(currentversion) Then
+            ' MsgBox("No new updates")
+        Else
+            MsgBox("An update is available!")
+            My.Computer.Network.DownloadFile("http://shieldspeak.com/eagle/Updatre.exe", appPath & "\updatre.exe")
+            Process.Start("updatre.exe")
+            Close()
+        End If
+    End Sub
+
+    Public Sub CheckDirectory()
+        tarkovPath = My.Computer.Registry.GetValue("HKEY_CURRENT_USER\E4GL3\ETE", "tarkovPath", Nothing)
+        If Not My.Computer.FileSystem.FileExists(tarkovPath) Then
+            MsgBox("Please select your ""Battlestate Games"" folder")
+            Dim folder As New FolderBrowserDialog
+            Dim flag As Byte = 0
+            '       Dim path As String
+            Do
+                If folder.ShowDialog = Windows.Forms.DialogResult.OK Then
+                    'MsgBox(folder.SelectedPath)
+                    path = folder.SelectedPath & "\BsgLauncher\BsgLauncher.exe"
+                    ' MsgBox(path)
+                    'Close()
+                    If My.Computer.FileSystem.FileExists(path) Then
+                        flag = 1
+                        My.Computer.Registry.SetValue("HKEY_CURRENT_USER\E4GL3\ETE", "tarkovPath", path)
+                    Else
+                        MsgBox("Cannot find game launcher!", 16)
+                        flag = 0
+                        path = ""
+                    End If
+
+                End If
+            Loop Until flag = 1
+            tarkovPath = My.Computer.Registry.GetValue("HKEY_CURRENT_USER\E4GL3\ETE", "tarkovPath", Nothing)
+
+        End If
+
+    End Sub
+
+End Class
